99d4065cece55467dd0cd652e772a28f07419805
ac044401 <AC044401@cerner.net>
2018-04-30 20:43:14 -0500

    Finished tutorial

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 var express = require('express');
     mongoose = require('mongoose');
+    bodyParser = require('body-parser');
 
-var db = mongoose.connect('mongodb+srv://diboy2:testing123@cluster0-fzlqn.mongodb.net/bookAPI');
+
+var db;
+if(process.env.ENV == 'Test'){
+    db = mongoose.connect('mongodb://localost/bookAPI_test');
+}
+else{
+    db = mongoose.connect('mongodb+srv://diboy2:testing123@cluster0-fzlqn.mongodb.net/bookAPI');
+}
 
 var Book = require('./models/bookModel');
 
@@ -9,37 +17,14 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
-var bookRouter  = express.Router();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
-bookRouter.route('/Books')
-    .get(function(req, res){
-        var query = {};
+bookRouter = require('./Routes/bookRoutes')(Book);
 
-        if (req.query.genre){
-            query.genre = req.query.genre;
-        }
-        Book.find(query, function(err, books){
-            if(err) {
-                res.status(500);
-            }
-            else {
-                res.json(books);
-            }
-        });
-    });
 
-bookRouter.route('/Books/:bookId')
-    .get(function(req, res){
-        Book.findById(req.params.bookId, function(err, book){
-            if(err){
-                res.status(500).send(err);
-            }
-            else{
-                res.json(book);
-            }
-        });
-    });
-app.use('/api', bookRouter);
+app.use('/api/books', bookRouter);
+// app.use('/api/authors', authorRouter);
 
 
 app.get('/', function(req, res){
@@ -49,3 +34,5 @@ app.get('/', function(req, res){
 app.listen(port, function(){
     console.log('Gulp is running on PORT: ' + port);
 });
+
+module.exports = app;
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
-    nodemon = require('gulp-nodemon');
+    nodemon = require('gulp-nodemon'),
+    gulpMocha = require('gulp-mocha'),
+    env = require('gulp-env'),
+    supertest = require('supertest');
 
 gulp.task('default', function(){
     nodemon({
@@ -15,4 +18,10 @@ gulp.task('default', function(){
     .en('restart', function(){
         console.log('Restarting');
     });
+});
+
+gulp.task('test', function(){
+    env({vars: {ENV: 'Test'}});
+    gulp.src('tests/*.js', {read: false})
+        .pipe(gulpMocha({reporter: 'nyan'}))
 });
